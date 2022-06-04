bb5b26c48f2043cea5d1390a5197c9b76e2bced4
Harsh Choudhary <harshchoudhary66669@gmail.com>
2022-06-04 19:20:54 +0530

    Day 5 completed

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -11,6 +11,18 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String name = "";
   bool changeState = false;
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formKey.currentState!.validate() ) {
+      setState(() {
+        changeState = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      Navigator.pushNamed(context, MyRoutes.homeRoute);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -34,71 +46,87 @@ class _LoginPageState extends State<LoginPage> {
               Padding(
                 padding:
                     const EdgeInsets.symmetric(vertical: 16, horizontal: 30),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                          //  border: InputBorder(borderSide: BorderStyle.solid)),
-                          hintText: "Enter username",
-                          labelText: "Username"),
-                      onChanged: (value) {
-                        name = value;
-                        setState(() {});
-                      },
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          hintText: "Enter password", labelText: "Password"),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    // inkwell makes container clickable, we can also use gesture detector but inkwell shows some click efffects.
-                    Material(
-                      color: changeState ? Colors.green : Colors.deepPurple,
-                      borderRadius: changeState? BorderRadius.circular(22): BorderRadius.circular(20),
-                      child: InkWell(
-                        onTap: () async {
-                          setState(() {
-                            changeState = true;
-                          });
-                          await Future.delayed(Duration(seconds: 1));
-                         // Navigator.pushNamed(context, MyRoutes.homeRoute);
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                            //  border: InputBorder(borderSide: BorderStyle.solid)),
+                            hintText: "Enter username",
+                            labelText: "Username"),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Username can't be empty";
+                          }
+
+                          return null;
                         },
-                        child: AnimatedContainer(
-                          duration: Duration(seconds: 1),
-                          width: changeState ? 40 : 150,
-                          height: 40,
-                          alignment: Alignment.center,
-                          child: changeState
-                              ? Icon(
-                                  Icons.done,
-                                  color: Colors.white,
-                                )
-                              : Text("Login",
-                                  style: TextStyle(
-                                    fontSize: 22,
-                                    fontStyle: FontStyle.italic,
-                                    color: Colors.white,
-                                  )),
-                          // decoration: BoxDecoration(
-                          //   shape: changeState
-                          //       ? BoxShape.circle
-                          //       : BoxShape.rectangle,
-                          //   color: changeState ? Colors.green : Colors.deepPurple,
-                          //   // borderRadius: BorderRadius.circular(22)
-                          // ),
-                        ),
+                        onChanged: (value) {
+                          name = value;
+                          setState(() {});
+                        },
                       ),
-                    )
-                    // ElevatedButton(
-                    //     style: TextButton.styleFrom(),
-                    //     onPressed: () {
-                    //       Navigator.pushNamed(context, MyRoutes.homeRoute);
-                    //     },
-                    //     child: Text("Sign in"))
-                  ],
+                      TextFormField(
+                        
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            hintText: "Enter password", labelText: "Password"),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Password can't be empty";
+                          } else if (value.length < 8) {
+                            return "Password length should be atleast 8";
+                          }
+
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      // inkwell makes container clickable, we can also use gesture detector but inkwell shows some click efffects.
+                      Material(
+                        color: changeState ? Colors.green : Colors.deepPurple,
+                        borderRadius: changeState
+                            ? BorderRadius.circular(22)
+                            : BorderRadius.circular(20),
+                        child: InkWell(
+                          onTap: () => moveToHome(context),
+                          child: AnimatedContainer(
+                            duration: Duration(seconds: 1),
+                            width: changeState ? 40 : 150,
+                            height: 40,
+                            alignment: Alignment.center,
+                            child: changeState
+                                ? Icon(
+                                    Icons.done,
+                                    color: Colors.white,
+                                  )
+                                : Text("Login",
+                                    style: TextStyle(
+                                      fontSize: 22,
+                                      fontStyle: FontStyle.italic,
+                                      color: Colors.white,
+                                    )),
+                            // decoration: BoxDecoration(
+                            //   shape: changeState
+                            //       ? BoxShape.circle
+                            //       : BoxShape.rectangle,
+                            //   color: changeState ? Colors.green : Colors.deepPurple,
+                            //   // borderRadius: BorderRadius.circular(22)
+                            // ),
+                          ),
+                        ),
+                      )
+                      // ElevatedButton(
+                      //     style: TextButton.styleFrom(),
+                      //     onPressed: () {
+                      //       Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      //     },
+                      //     child: Text("Sign in"))
+                    ],
+                  ),
                 ),
               )
             ],
