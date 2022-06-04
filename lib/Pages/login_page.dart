080e1d726a8844f76e5b4cb66823532838a43a6b
Harsh Choudhary <harshchoudhary66669@gmail.com>
2022-06-04 17:06:24 +0530

    Day 3 completed

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,13 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-          child: Container(
-        child: Text("Login Page"),
-      )),
-    );
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/login.png",
+                fit: BoxFit.contain,
+                height: 200,
+                width: 250,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "Welcome",
+                style: TextStyle(fontStyle: FontStyle.italic, fontSize: 24),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16, horizontal: 30),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                          //  border: InputBorder(borderSide: BorderStyle.solid)),
+                          hintText: "Enter username",
+                          labelText: "Username"),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          hintText: "Enter password", labelText: "Password"),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                        style: TextButton.styleFrom(),
+                        onPressed: () {
+                          Navigator.pushNamed(context, MyRoutes.homeRoute);
+                        },
+                        child: Text("Sign in"))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Pages/homepage.dart';
+import 'package:flutter_application_1/Pages/login_page.dart';
+import 'package:flutter_application_1/utils/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
+  
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  final int days = 13;
+   
+  // num data  type can store both int and double. use var for variable
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home : Material(
-        child: Center(
-          child: Container(
-      child: Text("Hi welcome to the $days days of flutter"),
-    ),
-        )
-      )
+     // home : Homepage(), app start hone p agr homepage khulna chie to ya to home : Homepage() esa likh do ya for line 25 m jsa likha h vsa dono m se koi ek
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily
+      ),
+      initialRoute: "/", // ye ni likhta  to "/" ye jis page k samne hota h vo page se app start hota
+      routes: {
+        "/" :(context) => LoginPage(),
+        MyRoutes.homeRoute:(context) => Homepage() ,// "/"" is used to set homepage as initial page
+        MyRoutes.loginRoute:(context) => LoginPage()
+      },
+      
     );
   }
 }
+
